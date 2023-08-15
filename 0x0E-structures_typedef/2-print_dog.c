4ed9c8dbdd9a00eeddb92f1df7aed7e5cfbf2978
proalx <profkaouthar@gmail.com>
2023-08-15 06:21:07 -0700

    betty style error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,15 +7,13 @@
  */
 void print_dog(struct dog *d)
 {
-	if(d==NULL)
+	if (d == NULL)
 		return;
-		
-	if(d->name==NULL)
-		d->name="(nil)";
-	if(d->owner==NULL)
-		d->owner="(nil)";
-	
-        printf("the name %s", d->name);
-	printf("age %f",d->age);
-	print("owner %s",d->owner);
+	if (d->name == NULL)
+		d->name =  "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("the name %s", d->name);
+	printf("age %f", d->age);
+	print("owner %s", d->owner);
 }
