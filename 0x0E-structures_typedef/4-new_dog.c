d122b7c2e61ef41c474f9388c6bad1fad2d17fd2
proalx <profkaouthar@gmail.com>
2023-08-15 07:21:49 -0700

    new code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
@@ -19,6 +20,7 @@ int _strlen(char *s)
 
 	return (i);
 }
+
 /**
  * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
@@ -28,7 +30,6 @@ int _strlen(char *s)
  *
  * Return: the pointer to dest
  */
-
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
@@ -48,6 +49,7 @@ char *_strcpy(char *dest, char *src)
 
 	return (dest);
 }
+
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
@@ -74,8 +76,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-
-dog->owner = malloc(sizeof(char) * (len2 + 1))
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
