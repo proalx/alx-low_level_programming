c7341457d771fba29099f6ee9668684f8c7d566b
kaouthar maarafi <kaoutharmaarafi@gmail.com>
2023-08-24 15:25:52 -0700

    first task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,21 +4,18 @@
 size_t print_list(const list_t *h)
 {
     size_t cmpt = 0;
-    
     while (h != NULL)
     {
-        if (h->str == NULL)
-	{
-            printf("[0] (nil)\n");
-        }
-       	else
-	{
-            printf("[%lu] %s\n", h->len, h->str);
-        }
-        cmpt++;
-
-        h = h->next;
+	    if (h->str == NULL)
+	    {
+		    printf("[0] (nil)\n");
+	    }
+	    else
+	    {
+		    printf("[%lu] %s\n", h->len, h->str);
+	    }
+	    cmpt++;
+	    h = h->next;
     }
-    
-    return cmpt;
+    return (cmpt);
 }
