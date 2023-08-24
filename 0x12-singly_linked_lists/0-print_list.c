566e6159042911393c4997cc8961f56d164b3135
kaouthar maarafi <kaoutharmaarafi@gmail.com>
2023-08-24 15:29:43 -0700

    first task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
+/**
+ * print_list-prints all the elements of a list_t list.
+ * @h: pointer
+ *
+ * Return:the number of nodes
+ */
 
 size_t print_list(const list_t *h)
 {
