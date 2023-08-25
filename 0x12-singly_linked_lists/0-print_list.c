0b387def2891ae1e4ca0829094dd8c1b9d9ade9d
kaouthar maarafi <kaoutharmaarafi@gmail.com>
2023-08-25 13:28:20 -0700

    reverse lines

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,18 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t cmpt = 0;
+    size_t cmpt;
+    cmpt = 0;
     while (h != NULL)
     {
 	    if (h->str == NULL)
-	    {
+	    
 		    printf("[0] (nil)\n");
-	    }
+	    
 	    else
-	    {
+	    
 		    printf("[%lu] %s\n", h->len, h->str);
-	    }
+	    
 	    h = h->next;
 	    cmpt++;
 	    
